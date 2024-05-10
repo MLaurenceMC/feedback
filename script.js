6209afa85242ad4b9cea5bbc76dc628e6b343aef
MLaurenceMC <143578936+MLaurenceMC@users.noreply.github.com>
2024-05-11 05:39:12 +0800

    added the switch to dynamic input graph

--- script.js
+++ script.js
@@ -20,6 +20,10 @@ document.getElementById('r').addEventListener('input', function() {
   updateGraph();
 });
 
+document.getElementById('switch').addEventListener('input', function(){
+  updateGraph();
+})
+
 function update_kVals(k1, k2, k3){
   document.getElementById('k1').innerHTML = Number(k1).toFixed(4);
   document.getElementById('k2').innerHTML = Number(k2).toFixed(4);
@@ -28,9 +32,9 @@ function update_kVals(k1, k2, k3){
 
 function updateGraph() {
   const { k1, k2, k3 } = translate(f, z, r);
-  const { step_data, feedback } = calculateGraphs(k1, k2, k3);
+  const { input_data, feedback } = calculateGraphs(k1, k2, k3);
   update_kVals(k1, k2, k3);
-  myChart.data.datasets[0].data = step_data;
+  myChart.data.datasets[0].data = input_data;
   myChart.data.datasets[1].data = feedback;
   myChart.update();
 }
@@ -42,30 +46,37 @@ function translate(f, z ,r) {
   return {k1, k2, k3};
 }
 
-function calculateGraphs(a, b, c) {
-  const step_data = [];
+function calculateGraphs(k1, k2, k3) {
+  // constrain k2
+  k2 = Math.max(k2, 1.1 * (T*T/4 + T*k1/2));
+  
+  // initialize variables at 0
+  const input_data = [];
   const feedback = [];
-  //initialize values to start at 0
   let prev_x = 0;
   let feedback_y = 0;
   let der_y = 0;
   
   for (let x = 0; x <= 10; x+=T) {
-    let step_y = x < 1 ? 0 : 5;
-    let der_x = (step_y - prev_x)/ T;
-    b = Math.max(b, 1.1 * (T*T/4 + T*a/2)); 
-    //b = Math.max(b, 0.5 + a/2, a); 
+    // input function calcuation
+    let input = 0;
+    if(document.getElementById('switch').checked){
+      input = x < 1 ? 0 : x < 2.5 ? 10 : x < 3.5 ? -5 : x < 4.5 ? 20*x-73 : x < 5.5 ? -20*x+103 : 0;
+    }else{
+      input = x <= 1 ? 0 : 5; //step function
+    }
+    input_data.push({x, y: input});
+    // feedback function calculation
+    let der_x = (input - prev_x)/ T;
     feedback_y = feedback_y + T * der_y;
-    der_y = der_y + T * (step_y + c * der_x - feedback_y - a * der_y)/ b;
-    prev_x = step_y;
-    //push the data
-    step_data.push({x, y: step_y });
+    der_y = der_y + T * (input + k3 * der_x - feedback_y - k1 * der_y)/ k2;
+    prev_x = input;
     feedback.push({x, y: feedback_y});
   }
-  return {step_data, feedback};
+  return {input_data, feedback};
 }
 
-const {step_data, feedback} = calculateGraphs(k1, k2, k3);
+const {input_data, feedback} = calculateGraphs(k1, k2, k3);
 
 const ctx = document.getElementById('myChart').getContext('2d');
 const myChart = new Chart(ctx, {
@@ -73,8 +84,8 @@ const myChart = new Chart(ctx, {
   data: {
     datasets: [
       {
-        label: 'Step Function',
-        data: step_data,
+        label: 'Input Function',
+        data: input_data,
         borderColor: 'rgb(75, 192, 192)',
         tension: 0.1
       },{
@@ -93,7 +104,7 @@ const myChart = new Chart(ctx, {
         position: 'bottom'
       },
       y: {
-        min: -5, max: 20,
+        min: -10, max: 20,
         type: 'linear',
         position: 'left'
       }
@@ -114,3 +125,4 @@ function adjustAspectRatio() {
 }
 window.addEventListener('resize', adjustAspectRatio);
 document.addEventListener('DOMContentLoaded', adjustAspectRatio);
+
